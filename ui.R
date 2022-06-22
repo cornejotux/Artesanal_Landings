5ff9240bf5991b834b8086478dcc38dd1ce326a6
Jorge Cornejo <cornejotux@gmail.com>
2022-06-22 10:16:07 -0400

    Se agregaron link a las RRSS.

--- ui.R
+++ ui.R
@@ -69,6 +69,21 @@ bootstrapPage(
         <a href='http://sitio.gorebiobio.cl/' target=_new>GORE Biobío</a>. <br>
      V1.0 2022/06"), 
         class="d-none d-lg-block"),
+      div(HTML('<center><a href="https://www.instagram.com/safa_ifop/" target="_blank" rel="noopener">
+            <img class="alignleft" src="https://cdn-icons-png.flaticon.com/512/1409/1409946.png" alt="Instagram" width="40" height="40" /></a>
+          <a href="https://twitter.com/SAFA_IFOP" target="_blank" rel="noopener">
+            <img class="" src="https://webtus.net/wp-content/uploads/2016/01/Twitter-icon.png.png" width="40" height="40" /></a>
+          <a href="https://www.facebook.com/profile.php?id=100081285572255" target="_blank" rel="noopener">
+            <img class="size-medium alignright" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu4iTr9XPJnxL83wRi5SD1obvTeSv9ArMLHg&amp;usqp=CAU" width="40" height="40" /></a></center>'),
+          class="d-none d-lg-block"),
+      div(HTML('<center><a href="https://www.instagram.com/safa_ifop/" target="_blank" rel="noopener">
+            <img class="alignleft" src="https://cdn-icons-png.flaticon.com/512/1409/1409946.png" alt="Instagram" width="20" height="20" /></a>
+          <a href="https://twitter.com/SAFA_IFOP" target="_blank" rel="noopener">
+            <img class="" src="https://webtus.net/wp-content/uploads/2016/01/Twitter-icon.png.png" width="20" height="20" /></a>
+          <a href="https://www.facebook.com/profile.php?id=100081285572255" target="_blank" rel="noopener">
+            <img class="size-medium alignright" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu4iTr9XPJnxL83wRi5SD1obvTeSv9ArMLHg&amp;usqp=CAU" width="20" height="20" /></a></center>'),        
+          class="d-block d-lg-none"),
+      
       HTML("<!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src='https://www.googletagmanager.com/gtag/js?id=G-VRMRJVHGYZ'></script>
         <script>
