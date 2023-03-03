4d556aaa495b47002506bb9def0496d7ab1a6c00
Jorge Cornejo <cornejotux@gmail.com>
2023-03-03 13:56:17 -0300

    Cambio el numero de filas que hay que hacer SKIP.

--- global.R
+++ global.R
@@ -27,13 +27,13 @@ doc.raw <- getURL(url)
 
 a <- str_split(doc.raw, "Pelágicos - Anchoveta-Sardina Común V-X")[[1]][1]
 
-aa <- str_split(a, "16_cuota_anchoveta-sardina_comun_v-x_2022")[[1]][2]
+aa <- str_split(a, "16_cuota_anchoveta-sardina_comun_v-x_2023")[[1]][2]
 aa3 <- str_split(aa, ".xlsx")[[1]][1]
 
-xlsUrl <- paste("http://www.sernapesca.cl/sites/default/files/16_cuota_anchoveta-sardina_comun_v-x_2022", 
+xlsUrl <- paste("http://www.sernapesca.cl/sites/default/files/16_cuota_anchoveta-sardina_comun_v-x_2023", 
                 aa3, ".xlsx", sep="")
 
-archivo <- paste('16_cuota_anchoveta-sardina_comun_v-x_2022', aa3, '.xlsx', sep="")
+archivo <- paste('16_cuota_anchoveta-sardina_comun_v-x_2023', aa3, '.xlsx', sep="")
 
 aa3 <- str_replace(aa3, "v", "")
 aa3 <- str_replace(aa3, "_", "")
@@ -43,7 +43,7 @@ if (!file.exists(paste(aa3, ".xlsx", sep="")))
   download.file(xlsUrl, paste(aa3, ".xlsx", sep=""), mode="wb")
 }
 
-ccAnchoveta <- read_excel(paste(aa3, ".xlsx", sep=""), sheet = "Anchoveta", skip = 2)
+ccAnchoveta <- read_excel(paste(aa3, ".xlsx", sep=""), sheet = "Anchoveta", skip = 4)
 
 ccAnchoveta2 <- ccAnchoveta %>% 
     filter(!is.na(...1)) %>%
@@ -68,7 +68,7 @@ for (i in 2:length(ccAnchoveta2$Región))
 }
 
 
-ccSardina <- read_excel(paste(aa3, ".xlsx", sep=""), sheet = "Sardina comun", skip = 2)
+ccSardina <- read_excel(paste(aa3, ".xlsx", sep=""), sheet = "Sardina comun", skip = 4)
 
 ccSardina2 <- ccSardina %>% 
   filter(!is.na(...1)) %>%
